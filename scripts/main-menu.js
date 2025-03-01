53f6a81a714d11845f957d8c79b006b212c46982
danielz0102 <danyzglez@hotmail.com>
2025-02-28 18:28:01 -0600

    agregar música en menú principal

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -1,9 +1,13 @@
 const VOLUME_ON_ICON = "assets/volume-on.svg";
 const VOLUME_OFF_ICON = "assets/volume-off.svg";
 const volume = document.querySelector(".sound-btn");
+const audio = document.querySelector("audio");
 
 volume.addEventListener("click", () => {
   const icon = volume.querySelector("img");
+
+  if (audio.paused) audio.play();
+  audio.muted = !audio.muted;
 
   if (icon.getAttribute("src") === VOLUME_ON_ICON) {
     icon.setAttribute("src", VOLUME_OFF_ICON);
